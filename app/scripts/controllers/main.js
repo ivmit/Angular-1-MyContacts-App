c30d461606a0f57599c88fb4b0d09561cb3b353b
nony1992 <ionutvalentin66@gmail.com>
2015-12-20 16:45:24 +0200

    Scaffolding the app using Yeoman

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name myContactsApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the myContactsApp
+ */
+angular.module('myContactsApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
